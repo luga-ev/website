86c5b5c117ddbc4d8b733d87e69b6ccc307caa42
Ingo Blechschmidt <iblech@web.de>
2016-05-27 14:08:31 +0200

    Webseite neu generiert (2016-05-27 14:08)

---
+++ download/Vortraege/1-1.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use warnings;    # Warnungen
+use strict;      # sauberes Programmieren
+use diagnostics; # ausfuehrliche Warnungen
+
+# Jetzt wollen wir auch etwas ausgeben
+print "Hallo Welt\n";
