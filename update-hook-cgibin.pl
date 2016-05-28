078b5a9cfacdab90a3cb3e127061c2f357f3ffd0
Ingo Blechschmidt <iblech@web.de>
2016-05-28 16:18:25 +0200

    Einfaches Rate-Limiting

--- update-hook-cgibin.pl
+++ update-hook-cgibin.pl
@@ -1,10 +1,16 @@
 #!/usr/bin/perl
-# Trivial CGI script to fetch the current version of the website
 
 use warnings;
 use strict;
 
 print "Content-Type: text/plain\015\012Content-Length: 4\015\012\015\012ok\015\012";
+
+chdir "/var/www/luga-preview" or exit;
+
+exit unless -M ".git" > 30/86400;
+# Only update once in thirty seconds.
+# Note: This duration has to be less than the time Travis CI takes to rebuild
+# and push the website.
 
 my $pid = fork();
 exit if not defined $pid;  # fork() didn't work
@@ -15,8 +21,7 @@ exit if $pid;              # this is the parent
 
 exec "bash", "-c", <<'EOF';
     {
-	cd /var/www/luga-preview
 	git reset --hard origin/gh-pages
 	git pull --rebase
-    } 2>&1 | mail -s "luga push-hook triggered $(date +%Y-%m-%d %H:%M)" iblech@speicherleck.de
+    } 2>&1 | mail -s "luga push-hook triggered $(date +'%Y-%m-%d %H:%M')" iblech@speicherleck.de
 EOF
