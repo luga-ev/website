05a23055d1065faddd51b78a4ebc7a64de966ca2
Ingo Blechschmidt <iblech@web.de>
2016-06-05 19:49:00 +0200

    Aktivierung nur bei gh-pages-Aktivität und Diff-Übermittlung

--- update-hook-cgibin.pl
+++ update-hook-cgibin.pl
@@ -3,10 +3,15 @@
 use warnings;
 use strict;
 
+# Read the first few bytes of HTTP POST data (if present), to crudely check
+# whether the received event pertains to the gh-pages branch.
+read STDIN, my $payload, 256;
+
 print "Content-Type: text/plain\015\012Content-Length: 4\015\012\015\012ok\015\012";
 
 chdir "/var/www/luga-preview" or exit;
 
+exit unless defined $payload and $payload =~ /gh-pages/;
 exit unless -M ".git" > 30/86400;
 # Only update once in thirty seconds.
 # Note: This duration has to be less than the time Travis CI takes to rebuild
@@ -22,6 +27,8 @@ exit if $pid;              # this is the parent
 exec "bash", "-c", <<'EOF';
     {
 	git reset --hard origin/gh-pages
-	git pull --rebase
+	git fetch
+	git diff gh-pages origin/gh-pages
+	git merge origin/gh-pages
     } 2>&1 | mail -s "luga push-hook triggered $(date +'%Y-%m-%d %H:%M')" iblech@speicherleck.de
 EOF
