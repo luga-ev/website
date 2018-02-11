33207f7c9d865c9d929f96ccab3397db1dea5c64
Ingo Blechschmidt <iblech@web.de>
2018-02-11 11:21:40 +0100

    Ignoriere einige bekannte Arten von Adressen

--- rewrite-http-to-https.pl
+++ rewrite-http-to-https.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -i -pw
-# Sehr einfach gestricktes Skript, um in Textdateien HTTP- in HTTPS-Links zu
-# ersetzen. Mit curl wird geprüft, ob die HTTPS-Alternativen funktionieren.
+# Simple script to substitute HTTP links by HTTPS links in text files.
+# Curl is used to check whether the potential HTTPS alternatives work.
 #
-# Aufruf: ./rewrite-http-to-https.pl text1.txt text2.txt ...
+# Usage: perl -i -pw rewrite-http-to-https.pl text1.txt text2.txt ...
 #
-# Oder parallel: for i in *.txt; do ./rewrite-http-to-https.pl "$i" & sleep 0.1; done
+# Or in parallel: for i in *.txt; do perl -i -pw rewrite-http-to-https.pl "$i" & sleep 0.1; done
 
 sub check_link {
     system(qw< curl -L -m 30 -f -o /dev/null -s -- >, $_[0]) == 0;
@@ -16,22 +16,27 @@ sub http2https {
     return $str;
 }
 
-s#(http://.*?)([\s\)\]}"<>])#
+next if /xmlns|\Q192.168.\E|\Qfd00:\E|localhost/;
+
+s|(http://.*?)([\s\)\]}"<>])|
     my $url       = $1;
     my $delimiter = $2;
 
-    print STDERR "... $url";
-
-    if(not check_link($url)) {
-        print STDERR "\033[2K\r404 $url\n";
+    if($url =~ /\Qwww.w3.org/) {  # is contained in XML headers
+        print STDERR "IGN $url\n";
     } else {
-        print STDERR "\033[2K\r... $url";
-        if(check_link(http2https($url))) {
-            print STDERR "\033[2K\rSEC $url\n";
-            $url = http2https($url);
+        print STDERR "... $url";
+        if(not check_link($url)) {
+            print STDERR "\033[2K\r404 $url\n";
         } else {
-            print STDERR "\033[2K\rUNS $url\n";
+            if(check_link(http2https($url))) {
+                print STDERR "\033[2K\rSEC $url\n";
+                $url = http2https($url);
+            } else {
+                print STDERR "\033[2K\rUNS $url\n";
+            }
         }
     }
+
     "$url$delimiter";
-#eg;
+|eg;
